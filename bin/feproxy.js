6fef63e651fb87c042cf2449276c11b0fdcc5d82
飞羏 <feixiang.yfx@alibaba-inc.com>
2018-11-09 15:19:01 +0800

    feat: add argvs

--- bin/feproxy.js
+++ bin/feproxy.js
@@ -3,12 +3,25 @@
 const path = require('path');
 const child_process = require('child_process');
 
-const app = path.join(__dirname, '../app.js');
+const argv = require('yargs')
+  .alias('v', 'version')
+  .describe('version', 'output the version number')
+  .help('help')
+  .argv;
 
-child_process.fork(app, {
-  env: {
-    ...process.env,
-    NODE_ENV: 'production',
-  },
-  stdio: 'inherit',
-});
+const app = path.join(__dirname, '../app.js');
+const pkg = require('../package.json');
+
+if (argv.version) {
+  console.log(pkg.version);
+} else if (argv.help) {
+  console.log(argv.help());
+} else {
+  child_process.fork(app, {
+    env: {
+      ...process.env,
+      NODE_ENV: 'production',
+    },
+    stdio: 'inherit',
+  });
+}
